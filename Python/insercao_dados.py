6efeacff30083c240842f01a8c97f67df8ac3894
unknown <thalesdefreitasferraz@gmail.com>
2023-02-19 14:19:06 -0300

    .

--- Python/insercao_dados.py
+++ Python/insercao_dados.py
@@ -1,1 +1,11 @@
-print('la')
+import pandas as pd
+import pyodbc
+
+dados_conexao = (
+    "Driver={SQL Server};"
+    "Server=DESKTOP-RFSFG8V;"
+    "Database=EmpresaLogistica;"
+)
+
+conexao  = pyodbc.connect(dados_conexao)
+print('Conexão bem sucedida')
