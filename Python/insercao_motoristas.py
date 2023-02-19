6a2809e8629fd97ad0e65af493c6a75709c96ccb
unknown <thalesdefreitasferraz@gmail.com>
2023-02-19 14:41:56 -0300

    adicionando novos arquivos

---
+++ Python/insercao_motoristas.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import pyodbc
+
+#  conexão com o banco de dados
+dados_conexao = (
+    "Driver={SQL Server};"
+    "Server=DESKTOP-RFSFG8V;"
+    "Database=EmpresaLogistica;"
+)
+
+conexao  = pyodbc.connect(dados_conexao)
+cursor = conexao.cursor()
+
+# importando as planilhas
+df = pd.read_excel('Excel/Cadastros.xlsx',sheet_name='Motoristas')
+n = df.shape[0]
+
+# percorrendo  a tabela
+for i in range(n):
+    SK_Motorista = df['SK_Motorista'][i]
+    Motorista = df['Motorista'][i]
+    script = f''' INSERT INTO CadMotoristas (SK_MOTORISTA,MOTORISTA) VALUES ('{SK_Motorista}', '{Motorista}')'''
+    cursor.execute(script)
+    cursor.commit()
