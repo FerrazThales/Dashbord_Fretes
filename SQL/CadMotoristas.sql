e97738f0ea35be3e50e5d2524861a241591e55e5
unknown <thalesdefreitasferraz@gmail.com>
2023-02-19 14:40:17 -0300

    atualizando os códigos

--- SQL/CadMotoristas.sql
+++ SQL/CadMotoristas.sql
@@ -2,6 +2,5 @@ CREATE TABLE CadMotoristas
 (
 	ID_TABLE INT PRIMARY KEY IDENTITY (1,1),
 	SK_Motorista VARCHAR(50) NOT NULL,
-	Motorista VARCHAR(100),
-	HORARIO_INSERCAO datetime default getdate()
+	Motorista VARCHAR(100)
 )
