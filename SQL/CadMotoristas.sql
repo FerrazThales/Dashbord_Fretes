8e7182e25a2d5cd49803db816bb09f4429aedcfb
unknown <thalesdefreitasferraz@gmail.com>
2023-02-19 13:06:02 -0300

    Adicionando novos arquivos

--- SQL/CadMotoristas.sql
+++ SQL/CadMotoristas.sql
@@ -2,5 +2,6 @@ CREATE TABLE CadMotoristas
 (
 	ID_TABLE INT PRIMARY KEY IDENTITY (1,1),
 	SK_Motorista VARCHAR(50) NOT NULL,
-	Motorista VARCHAR(100)
+	Motorista VARCHAR(100),
+	HORARIO_INSERCAO datetime default getdate()
 )
