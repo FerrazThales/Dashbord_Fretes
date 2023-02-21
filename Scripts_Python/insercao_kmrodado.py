bd745730cb0a6fbd64960f22785f5b9cfce22b62
unknown <thalesdefreitasferraz@gmail.com>
2023-02-21 11:59:24 -0300

    Adicionando novos códigos

--- Scripts_Python/insercao_kmrodado.py
+++ Scripts_Python/insercao_kmrodado.py
@@ -13,30 +13,31 @@ cursor = conexao.cursor()
 
 # importando as planilhas
 cabecalho = ['Mês','SK_Veiculo','SK_Motorista','Km Percorrido','Litros','Combustível','Manutenção','Custo Fixo']
-df = pd.read_excel('fKMRodado.xlsx',names = cabecalho)
+df = pd.read_excel('Base_de_Dados/fKMRodado.xlsx',names = cabecalho)
 df.dropna(subset=cabecalho[1:8], inplace=True)
 df = df[~df.isin(cabecalho).any(axis=1)]
+df['Mês'] = pd.to_datetime(df['Mês'])
 n = df.shape[0]
 
 # percorrendo  a tabela
 for i in range(n):
     # ajustando as datas para 2021 e 2022
-    data = (df['Data'][i]).strftime('%Y-%m-%d')
-    SK_Cliente = df['SK_Cliente'][i]
+    data = (df['Mês'][i]).strftime('%Y-%m-%d')
     SK_Veiculo = df['SK_Veiculo'][i]
-    doc_fiscal  = df['Numero Documento Fiscal'][i]
-    cod_viagem  = df['Viagem'][i]
-    cod_IBGE  = df['Cod IBGE'][i]
-    valor_frete  = df['Valor do Frete Líquido'][i]
-    peso_kg  = df['Peso (KG)'][i]
-    peso_cubo  = df['Peso (Cubado)'][i]
-    valor_mercadoria  = df['Valor da Mercadoria'][i]
+    SK_Motorista = df['SK_Motorista'][i]
+    km_percorrido  = float(df['Km Percorrido'][i])
+    Litros  = float(df['Litros'][i])
+    comb  = float(df['Combustível'][i])
+    manut  = float(df['Manutenção'][i])
+    custo_fixo  = float(df['Custo Fixo'][i])
+    
     script = f''' 
         INSERT INTO Fretes 
         (DATA_KM,SK_VEICULO,SK_Motorista,KM_PERCORRIDO,LITROS,COMBUSTIVEL,MANUTENCAO,CUSTO_FIXO)
         VALUES 
-        ('{data}','{SK_Cliente}','{SK_Veiculo}','{doc_fiscal}','{cod_viagem}','{cod_IBGE}',
-        {valor_frete},{peso_kg},{peso_cubo},{valor_mercadoria})'''
+        ('{data}','{SK_Veiculo}','{SK_Motorista}',{km_percorrido},{Litros},{comb},{manut},{custo_fixo})
+        
+        '''
 
     
     cursor.execute(script)
